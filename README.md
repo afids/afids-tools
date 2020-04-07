891996975c4cfd279140fa6b9d3b031117a488e4
Jonathan Lau <jclau@raprl.local>
2020-04-07 10:04:05 -0400

    legacy scripts from original afids paper
    plan is to include here and generalize for other purposes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # afids-tools
 
 General tools for the afids project including creating an interface between fcsv files and ants.
+
+Goal is to centralize scripts used for converting to and from Slicer using ants (or other transform types).
+
---
+++ legacy/PHASE3_04_transform_fcsv_nlin.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# nonlinear transform of point fcsv files
+# pre-req: fmriprep output
+#~/GitHub/afids-analysis/scripts/PHASE3_04_transform_fcsv_nlin.sh -i ~/graham/project/fid_study_bids/PHASE2/input_data/OAS1_bids_MR1/participants.tsv >& ~/GitHub/afids-analysis/logs/PHASE3_04_transform_fcsv_nlin.log
+
+function usage {
+ echo ""
+ echo "Transform fcsv files"
+ echo ""
+ echo "Required args:"
+ echo "  -i input_csv with subject names (e.g. sub-C020)"
+ echo ""
+}
+
+if [ "$#" -lt 2 ]
+then
+ usage
+ exit 1
+fi
+
+while getopts "i:" options; do
+ case $options in
+     i ) 
+         in_csv=$OPTARG;;
+
+    * ) usage
+        exit 1;;
+ esac
+done
+
+input_bids_dir=~/graham/project/fid_study_bids/PHASE2/input_data/OAS1_bids_MR1/derivatives/fmriprep_1.1.1/fmriprep/
+input_fcsv_dir=~/GitHub/afids-analysis/data/PHASE2_output_afid_postQC/
+output_dir=~/GitHub/afids-analysis/data/PHASE3_output_afid_nlin/
+
+mkdir -p $output_dir
+{
+  read # first line is a header
+while read SUBJ; do
+  echo "---------"
+  echo $SUBJ
+  echo "---------"
+
+  input_warp_to_MNI152=${input_bids_dir}/${SUBJ}/anat/${SUBJ}_T1w_target-MNI152NLin2009cAsym_warp.h5
+  input_warp_to_MNI152_inv=${input_bids_dir}/${SUBJ}/anat/${SUBJ}_T1w_space-MNI152NLin2009cAsym_target-T1w_warp.h5
+  input_subj_id=`echo $SUBJ | cut -d "-" -f 2`
+  input_fcsv=${input_fcsv_dir}/OAS1_${input_subj_id}_MR1_T1_MEAN.fcsv
+
+  output_fcsv=${output_dir}/OAS1_${input_subj_id}_MR1_T1_MEAN_transformed_nlin.fcsv
+
+  echo python3 ~/graham/GitHub/sandbox/antsApplyH5TransformsToSlicerFCSV.py -i $input_fcsv -o $output_fcsv -f $input_warp_to_MNI152 -b $input_warp_to_MNI152_inv
+  python3 ~/GitHub/sandbox/antsApplyH5TransformsToSlicerFCSV.py -i $input_fcsv -o $output_fcsv -f $input_warp_to_MNI152 -b $input_warp_to_MNI152_inv
+
+done
+} < "$in_csv"
